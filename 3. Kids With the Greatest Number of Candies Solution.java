cf6de2db9684b5c2528ec24d758307ffebd11038
Puneet Sawhaney <153997477+puneetSawhaney@users.noreply.github.com>
2023-12-18 14:43:47 +0530

    Create 3. Kids With the Greatest Number of Candies Solution.java

---
+++ 3. Kids With the Greatest Number of Candies Solution.java
@@ -0,0 +1,22 @@
+// code by puneet
+// please visit explanation file to understand the approach I followed
+
+// question asked in Amazon Adobe Apple
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        List<Boolean> ans = new ArrayList<>();
+
+        int max = candies[0];
+
+        for(int i=0; i < candies.length; i++){
+            max = Math.max(max, candies[i]);
+        }
+
+        for(int i =0; i < candies.length; i++){
+            ans.add(candies[i] + extraCandies >= max);
+        }
+        return ans;
+    }
+}
