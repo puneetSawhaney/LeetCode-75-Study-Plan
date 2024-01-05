70e2de99b67b75a4abcd73d8ee7b2991e508ff8b
Puneet Sawhaney <153997477+puneetSawhaney@users.noreply.github.com>
2024-01-05 08:46:22 +0530

    Create 19. Find Pivot Index Solution.java

---
+++ 19. Find Pivot Index Solution.java
@@ -0,0 +1,22 @@
+// code by Puneet 
+// please visit to explanation file of this code to understand the approach
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        int[] pre = new int[nums.length];
+        int sum = nums[0];
+        pre[0] = 0;
+        for(int i =1; i < nums.length; i++){
+            sum +=nums[i];
+            pre[i] = pre[i-1] + nums[i-1];
+        }
+        for(int i =0; i < nums.length; i++){
+            if(pre[i] == sum - nums[i] - pre[i]){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
