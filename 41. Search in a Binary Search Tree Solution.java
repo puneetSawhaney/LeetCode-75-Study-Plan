d9044de9665a487e73540e1810dc403cf2f810eb
Puneet Sawhaney <153997477+puneetSawhaney@users.noreply.github.com>
2024-01-09 08:50:37 +0530

    Create 41. Search in a Binary Search Tree Solution.java

---
+++ 41. Search in a Binary Search Tree Solution.java
@@ -0,0 +1,32 @@
+// code by Puneet 
+// please visit to explanation file of this code to understand the approach
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        // iterate over root till null
+        while(root != null){
+            if(root.val == val){
+                return root;
+            }else if(root.val < val){
+                root = root.right;
+            }else{
+                root = root.left;
+            }
+        }
+        return null;
+    }
+}
